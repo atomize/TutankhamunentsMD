66f074a4573f67c146b49a72831b1cb28a83d453
atomize <63r71@tuta.io>
2019-04-01 16:14:15 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,140 @@
+const fetch = require('node-fetch');
+const fs = require('fs');
+const jsdom = require("jsdom");
+const {
+  JSDOM
+} = jsdom;
+const argv = require('minimist')(process.argv.slice(2))
+const options = {
+  runScripts: "dangerously",
+  resources: "usable",
+}
+
+function injectIIFE(dom) {
+  let evalFunction = `
+  let reqNumber = 0;
+  let doneNumber = 0;
+(function bam() {
+  function SendMessage(e) {
+    if (window.CustomEvent) {
+        var event = new CustomEvent("newMessage", {
+            bubbles: true,
+            cancelable: true
+        });
+        window.dispatchEvent(event);
+    }
+  }
+    var origOpen = XMLHttpRequest.prototype.open;
+    XMLHttpRequest.prototype.open = function () {
+        console.log('request started!');
+        reqNumber++
+        this.addEventListener('load', function () {
+            console.log('request completed!');
+           doneNumber++
+            if (reqNumber == doneNumber){
+              SendMessage(document)
+              console.log('message sent')
+            }
+        });
+        origOpen.apply(this, arguments);
+    };
+})();`
+  dom.window.eval(evalFunction)
+  return dom
+}
+
+function manipulateDOM(dom) {
+  let allPromises;
+  dom.window.addEventListener("newMessage", newMessageHandler, false);
+
+  const fetcher = (eachFile) => {
+    return fetch(eachFile)
+      .then(response => {
+        return response.buffer();
+      })
+  }
+  const filePromises = (eachFile) => {
+    return new Promise((resolve, reject) => {
+      fs.readFile(eachFile, (err, data) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(data)
+        }
+      })
+    })
+  }
+
+  function newMessageHandler() {
+    let sourceArray = []
+    Array.prototype.forEach.call(dom.window.document.querySelectorAll('link[rel="stylesheet"]'), function (element) {
+      let el;
+      if (argv.file) {
+        el = element.href.replace('file:\/\/', '')
+      } else if (argv.url) {
+        el = element.href
+      }
+      sourceArray.push(el);
+      element.parentNode.removeChild(element)
+    });
+    Array.prototype.forEach.call(dom.window.document.querySelectorAll('script'), function (element) {
+      element.parentNode.removeChild(element)
+    });
+
+    if (argv.file) {
+      allPromises = sourceArray.map(filePromises)
+    } else if (argv.url) {
+      allPromises = sourceArray.map(fetcher);
+    }
+
+    Promise.all(allPromises).then(onfulfilled => {
+      const totalBufferContent = Buffer.concat(onfulfilled)
+      return totalBufferContent.toString()
+    }).then((buf) => {
+      let styleEl = dom.window.document.createElement('STYLE')
+      styleEl.textContent = buf
+      dom.window.document.head.appendChild(styleEl)
+      if (!argv.output) {
+        argv.output = 'index.static.html'
+        fs.writeFile(argv.output, dom.serialize(), (err) => {
+          if (err) throw err;
+          console.log('Done')
+        })
+
+      } else if (argv.output == 'github') {
+        if (!argv.username && !argv.password) {
+          return
+        }
+        let base64dom = dom.window.btoa(dom.serialize())
+        console.log(base64dom)
+        let postParams = {
+          "content": base64dom,
+          "encoding": "base64"
+        }
+
+        function submitBlob(opts) {
+          ChromeSamples.log('Posting request to GitHub API...');
+          fetch(argv.url, {
+            method: 'post',
+            body: JSON.stringify(opts)
+          }).then(function (response) {
+            return response.json();
+          }).then(function (data) {
+            ChromeSamples.log('Created Gist:', data.html_url);
+          });
+        }
+      }
+
+    })
+
+  }
+}
+
+if (argv.file) {
+  JSDOM.fromFile(argv.file, options).then(injectIIFE).then(manipulateDOM)
+} else if (argv.url) {
+  JSDOM.fromURL(argv.url, options).then(injectIIFE).then(manipulateDOM)
+} else {
+  console.log('gimme a file!')
+  return
+};
